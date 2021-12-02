a8aeb2e2c274175622dba3a67ee219151091d237
kampersanda <shnsk.knd@gmail.com>
2021-12-02 23:20:42 +0900

    add comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! # `sucds`: Succinct data structures in Rust
+//!
+//! `sucds` contains some succinct data structures written in Rust.
+//!
+//! ## Data structures
+//!
+//! So far, the following data structures are implemented. All of them are yet another Rust ports of implementations of [C++ Succinct library by Ottaviano](https://github.com/ot/succinct).
+//!
+//! - `BitVector`
+//!   - Bit vector in a plain format, supporting some utilities such as update, chunking, and predecessor queries.
+//! - `RsBitVector`
+//!   - Rank/select data structure over bit vector through Vigna's rank9 and hinted selection techniques.
+//! - `EliasFano`
+//!   - Compressed monotone sequence through Elias-Fano encoding.
+//! - `DArray`
+//!   - Constant-time select data structure over integer set through dense array technique by Okanohara and Sadakane.
 pub mod bit_vector;
 pub mod broadword;
 pub mod darray;
