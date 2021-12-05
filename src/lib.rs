b309a455718ff099a317928c57c4982d168ae73b
kampersanda <shnsk.knd@gmail.com>
2021-12-05 14:59:36 +0900

    add links

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,13 @@
 //!
 //! So far, the following data structures are implemented. All of them are yet another Rust ports of implementations of [C++ Succinct library by Ottaviano](https://github.com/ot/succinct).
 //!
-//! - `BitVector`
+//! - [`BitVector`](https://docs.rs/sucds/latest/sucds/bit_vector/struct.BitVector.html)
 //!   - Bit vector in a plain format, supporting some utilities such as update, chunking, and predecessor queries.
-//! - `RsBitVector`
+//! - [`RsBitVector`](https://docs.rs/sucds/latest/sucds/rs_bit_vector/struct.RsBitVector.html)
 //!   - Rank/select data structure over bit vector through Vigna's rank9 and hinted selection techniques.
-//! - `EliasFano`
+//! - [`EliasFano`](https://docs.rs/sucds/latest/sucds/elias_fano/struct.EliasFano.html)
 //!   - Compressed monotone sequence through Elias-Fano encoding.
-//! - `DArray`
+//! - [`DArray`](https://docs.rs/sucds/latest/sucds/darray/struct.DArray.html)
 //!   - Constant-time select data structure over integer set through dense array technique by Okanohara and Sadakane.
 pub mod bit_vector;
 pub mod broadword;
