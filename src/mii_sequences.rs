005ee1e309cdc8bf260876252d3d2687ec2ea4cc
kampersanda <shnsk.knd@gmail.com>
2023-03-21 17:38:24 +0900

    wip

--- src/mii_sequences.rs
+++ src/mii_sequences.rs
@@ -1,5 +1,35 @@
+//! Top module for monotone-increasing integer sequences.
 //!
+//! # Introduction
+//!
+//! *Monotone-increasing integer sequences* are a generalization of bit vectors, which are a multiset variant of $`S`$.
+//! More simply, it is a sorted array of integers.
+//!
+//! Let $`X = (x_0, x_1, \dots, x_{n-1})`$ be a sequence of $`n`$ integers
+//! such that $`0 \leq x_0`$, $`x_i \leq x_{i+1}`$, and $`x_{n-1} < u`$ for a universe $`u`$.
+//! Our sequences support the following queries:
+//!
+//! - $`\textrm{Rank}(x)`$ returns the number of elements $`x_k \in X`$ such that $`x_k < x`$ (implemented by [`Ranker`]).
+//! - $`\textrm{Select}(k)`$ returns $`x_k`$ (implemented by [`Selector`]).
+//! - $`\textrm{Predecessor}(x)`$ returns the largest element $`x_k \in X`$ such that $`x_k \leq x`$ (implemented by [`Predecessor`]).
+//! - $`\textrm{Successor}(x)`$ returns the smallest element $`x_k \in X`$ such that $`x \leq x_k`$ (implemented by [`Successor`]).
+//!
+//! Note that they are not limited depending on the data structures.
+//!
+//! # Data structures
+//!
+//! The implementations provided in this crate are summarized below:
+//!
+//! | Implementations | [Rank](Ranker) | [Select](Selector) | [Pred](Predecessor)/[Succ](Successor) | Memory (bits) |
+//! | --- | :-: | :-: | :-: | :-: |
+//! | [`EliasFano`] | $`O(\lg \frac{u}{n})`$ | $`O(1)`$ | $`O(\lg \frac{u}{n})`$ | $`n \lceil \lg \frac{u}{n} \rceil + 2n + o(n)`$ |
+//!
+//! ## Elias-Fano encoding
+//!
+//! [`EliasFano`] is an efficient data structure for sparse sequences (i.e., $`n \ll u`$).
+//! In addition to the basic queires listed above, this provides several access queries such as binary search.
 pub mod elias_fano;
+pub mod prelude;
 
 pub use elias_fano::EliasFano;
 pub use elias_fano::EliasFanoBuilder;
