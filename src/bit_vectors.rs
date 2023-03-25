1bdb5c8b71a5e2a55952f1d3e6beda82384c5381
kampersanda <shnsk.knd@gmail.com>
2023-03-25 15:11:58 +0900

    fix

--- src/bit_vectors.rs
+++ src/bit_vectors.rs
@@ -114,29 +114,30 @@ pub trait BitGetter {
     fn get_bit(&self, pos: usize) -> Option<bool>;
 }
 
-/// Interface for rank queries on monotone-increasing integer sequences.
+/// Interface for rank queries on bit vectors.
 ///
-/// Let $`X = (x_0, x_1, \dots, x_{n-1})`$ be a sequence of $`n`$ integers
-/// such that $`0 \leq x_0`$, $`x_i \leq x_{i+1}`$, and $`x_{n-1} < u`$ for a universe $`u`$.
+/// Let $`S \subseteq \{ 0,1,\dots,u-1 \}`$ be a set of positions
+/// at which bits are set in a bit vector of length $`u`$.
 pub trait Ranker {
-    /// Returns the number of elements $`x_k \in X`$ such that $`x_k < x`$,
+    /// Returns the cardinality of $`\{ x \in S \mid x < i \}`$,
     /// or [`None`] if $`u < x`$.
     fn rank1(&self, x: usize) -> Option<usize>;
 
-    /// Returns the number of integers $`x' \not\in X`$ such that $`0 \leq x' < x`$,
+    /// Returns the cardinality of $`\{ x \not\in S \mid 0 \leq x < i \}`$,
     /// or [`None`] if $`u < x`$.
     fn rank0(&self, x: usize) -> Option<usize>;
 }
 
-/// Interface for select queries on monotone-increasing integer sequences.
+/// Interface for select queries on bit vectors.
 ///
-/// Let $`X = (x_0, x_1, \dots, x_{n-1})`$ be a sequence of $`n`$ integers
-/// such that $`0 \leq x_0`$, $`x_i \leq x_{i+1}`$, and $`x_{n-1} < u`$ for a universe $`u`$.
+/// Let $`S \subseteq \{ 0,1,\dots,u-1 \}`$ be a set of positions
+/// at which bits are set in a bit vector of length $`u`$.
 pub trait Selector {
-    /// Returns $`x_k`$, or [`None`] if $`n \leq k`$.
+    /// Returns the $`k`$-th smallest position in $`S`$, or
+    /// [`None`] if out of bounds.
     fn select1(&self, k: usize) -> Option<usize>;
 
-    /// Returns the $`k`$-th smallest integer $`x`$ such that $`x \not\in X`$ and $`0 \leq x < u`$, or
+    /// Returns the $`k`$-th smallest integer $`x`$ such that $`x \not\in S`$ and $`0 \leq x < u`$, or
     /// [`None`] if out of bounds.
     fn select0(&self, k: usize) -> Option<usize>;
 }
